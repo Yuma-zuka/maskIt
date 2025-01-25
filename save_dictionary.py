ce9b09bfae644abc5701617893db90c10581137c
Yuma-zuka <yuma@MacBook-Air.local>
2025-01-25 20:20:59 +0900

    認証画像登録の処理をする画像を選択するシステム作成

--- save_dictionary.py
+++ save_dictionary.py
@@ -1,5 +1,8 @@
 import numpy as np
 import cv2
+import tkinter as tk
+import tkinter.filedialog, tkinter.messagebox
+import os
 
 #複数のsampleを入れれるようにする
 # recimage = cv2.imread("/Users/yuma/opencv/yumaFace.JPG")
@@ -7,10 +10,23 @@ import cv2
 
 
 class Dictionary:
-    def __init__(self):    
+    def __init__(self):
+
         self.face_detector = cv2.FaceDetectorYN_create("/Users/yuma/opencv/yunet_n_640_640.onnx", "", (320, 320))
         # FaceRecognizerSF の生成
         self.face_recognizer = cv2.FaceRecognizerSF_create("/Users/yuma/opencv/face_recognition_sface_2021dec.onnx", "")
+
+        self.root = tk.Tk()
+        self.root.withdraw()
+        # 表示するファイルの拡張子を限定する
+        self.file_Type = [("","*.JPG"),("","*.jpeg"),("","*.jpg"),("","*.PNG"),("","*.png"),("","*.HEIC")]
+        # 表示する初期ディレクトリーを指定する
+        self.userDirectry = os.path.expanduser("~")
+        tkinter.messagebox.showinfo('','処理ファイルを選択')
+        self.file = tkinter.filedialog.askopenfilenames(filetypes = self.file_Type,initialdir = self.userDirectry)
+        self.list = list(self.file)
+        # tkinter.messagebox.showinfo('○×プログラム',self.list)
+
 
     def recSave(self, image):
         recimage = cv2.imread(image)

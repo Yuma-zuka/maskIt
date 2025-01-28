8b4e3296298f79c2ea9532db8149dc47be7a2d86
Yuma-zuka <yuma@MacBook-Air.local>
2025-01-28 18:54:29 +0900

    機能ごとに拡張子を限定し実行するシステムを構築するための土台を作成

--- choice_rec_file.py
+++ choice_rec_file.py
@@ -3,22 +3,55 @@ import tkinter.filedialog, tkinter.messagebox
 import os
 import save_dictionary
 import guihome # type: ignore
+import imagerecpro
+import movierecpro
+from work_enum import Work #type: ignore
 
 class Dictionary:
-    def __init__(self):
+    def __init__(self, work):
+        self.work = work
+        # 表示するファイルの拡張子を限定する
+        self.image_extension = [("","*.JPG"),("","*.jpeg"),("","*.jpg"),("","*.PNG"),("","*.png"),("","*.HEIC")]
+        self.movie_extension = [("",".*MP4"),("","*.mp4"),("",".*MOV"),("",".*mov")]
+
         self.root = tk.Tk()
         self.root.withdraw()
-        # 表示するファイルの拡張子を限定する
-        self.file_Type = [("","*.JPG"),("","*.jpeg"),("","*.jpg"),("","*.PNG"),("","*.png"),("","*.HEIC")]
+
+        # ----------------------- #
+        # 機能ごとに拡張子の限定
+        # 実行対象ファイルの選択
+        # 実行クラスのインスタンス生成
+        # 実行
+        #  ---------------------- #
+        match self.work:
+            case Work.REGISTER:
+                self.file_Type = self.image_extension
+                self.ask_file()
+                save_file = save_dictionary.Register()
+                save_file.recSave(self.file)
+            case Work.MANAGEMENT:
+                self.file_Type = self.image_extension
+                self.ask_file()
+                # 登録した顔のデータファイルの管理クラスの呼び出し
+            case Work.IMAGE:
+                self.file_Type = self.image_extension
+                self.ask_file()
+                rec_file = imagerecpro.ImageRecPro()
+                rec_file.rec_image()
+            case Work.MOVIE:
+                self.file_Type = self.movie_extension
+                self.ask_file()
+                rec_file = movierecpro.MovieRecPro()
+                rec_file.rec_Movie()
+
+    def ask_file (self):
         # 表示する初期ディレクトリーを指定する
         self.userDirectry = os.path.expanduser("~")
         tkinter.messagebox.showinfo('','処理ファイルを選択')
         self.file = tkinter.filedialog.askopenfilename(filetypes = self.file_Type,initialdir = self.userDirectry)
-        save_file = save_dictionary.Register()
-        if (self.file != ""):
-            save_file.recSave(self.file)
-        else:
+        if (self.file == ""):
             guihome.Homewindow()
 
 if __name__ == "__main__":
-    save_data = Dictionary()
+    # save_data = Dictionary(Work.REGISTER)
+    save_data = Dictionary(Work.IMAGE)

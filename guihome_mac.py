71b22c27c68162478d689b1dd0bd43f6fcbd1ede
Yuma-zuka <yuma@MacBook-Air.local>
2025-02-05 09:55:09 +0900

    guihomeのmac対応版

--- guihome_mac.py
+++ guihome_mac.py
@@ -1,9 +1,9 @@
 from tkmacosx import Button
 import tkinter as tk
-import save_dictionary # type: ignore
-import imagerecpro
-import movierecpro
-import recyunet # type: ignore
+import choice_rec_file
+import real_time
+import management_data
+from work_enum import Work
 
 
 
@@ -11,17 +11,17 @@ class Homewindow:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("faceMozaicApp")
-        self.root.geometry("1000x800+200+50")
+        self.root.geometry("1440x847+0+0")
         self.root.resizable(False, False)
         self.root.configure(bg="#fafaf2")
 
         self.registration_process_btn = Button(self.root, text="register", command=self.do_registration_process, bg="#1995ad", fg="#537072", font=("Helvetica", 120))
         self.registration_process_btn.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
 
-        self.image_process_btn = Button(self.root, text="image", command=self.do_imagerecpro, bg="#a1d6e2", fg="#537072", font=("Helvetica", 120))
+        self.image_process_btn = Button(self.root, text="manage", command=self.do_management, bg="#a1d6e2", fg="#537072", font=("Helvetica", 120))
         self.image_process_btn.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
 
-        self.movie_process_btn = Button(self.root, text="movie", command=self.do_movierecpro, bg="#a1d6e2", fg="#537072",  font=("Helvetica", 120))
+        self.movie_process_btn = Button(self.root, text="image", command=self.do_imagerecpro, bg="#a1d6e2", fg="#537072",  font=("Helvetica", 120))
         self.movie_process_btn.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
 
         self.real_time_process_btn = Button(self.root, text="realTime", command=self.do_real_time_rec, bg="#a1d6e2", fg="#537072", font=("Helvetica", 120))
@@ -30,22 +30,19 @@ class Homewindow:
         self.root.mainloop()
 
     def do_registration_process(self):
-        self.root.withdraw()
-        registration = save_dictionary.Dictionary()
-        registration.recSave("/Users/yuma/opencv/yumaFace2.png")
+        self.root.destroy()
+        choice_rec_file.Dictionary(Work.REGISTER)
+    def do_management(self):
+        self.root.destroy()
+        management = management_data.ManageData()
+        management.manage()
     def do_imagerecpro(self):
-        self.root.withdraw()
-        imagerp = imagerecpro.ImageRecPro()
-        imagerp.rec_image()
-    def do_movierecpro(self):
-        self.root.withdraw()
-        movierp = movierecpro.MovieRecPro()
-        movierp.movierecpro()
+        self.root.destroy()
+        choice_rec_file.Dictionary(Work.IMAGE)
     def do_real_time_rec(self):
-        self.root.withdraw()
-        realtimerp = recyunet.RealTimeRec()
-        realtimerp.rec_real_time()
-        
+        self.root.destroy()
+        real_time_rec = real_time.BootRealTime()
+        real_time_rec.boot()
 
 if __name__ == "__main__":
     window = Homewindow()
